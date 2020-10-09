bc6ca9c5fda680abfa76f7cd9a72fb7f55674654
Scott <Scott@SCOTT-PC>
2020-10-09 05:59:02 -0700

    /

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://gentle-springs-11724.herokuapp.com/api/',
+  API_ENDPOINT: 'https://gentle-springs-11724.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 }
